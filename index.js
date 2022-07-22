88095badc9d926535d3bb6f7e2a3ff125ce611bb
Emmanuelnz <emmanuelnun75@gmail.com>
2022-07-22 04:12:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -382,5 +382,5 @@ app.use((err, req, res, next) => {
 // Listen for request
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
-  console.log('Listening on Port ' + port);
+ console.log('Listening on Port ' + port);
 });
