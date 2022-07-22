4232165b01f60b1290d016d106e0c0787945c6bb
Emmanuelnz <emmanuelnun75@gmail.com>
2022-07-22 04:30:15 -0400

    fixed link issue

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const Users = Models.User;
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Online
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let users = [
 ]
