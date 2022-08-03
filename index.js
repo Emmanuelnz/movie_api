2b59f980e483617673eebef783f3d9010edeb097
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-02 23:12:59 -0500

    Fixed reference issue

--- index.js
+++ index.js
@@ -109,8 +109,10 @@ app.put('/users/:Username',
       if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
       }
+    
+    let hashedPassword = Users.hashPassword(req.body.Password);
 
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
       Password: hashedPassword,
