ac3cb533aa608cb61c49394eb2f5626b7718c976
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-09 17:48:12 -0500

    updated to allow all origins

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ const {check, validationResult} = require('express-validator');
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080',];
-app.use(cors({
-origin: (origin, callback) => {
-  if(!origin) return callback(null, true);
-  if(allowedOrigins.indexOf(origin) === -1){
-    let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-    return callback(new Error(message ), false);
-  }
-  return callback(null, true);
-}
-}));
+// All origins
+app.use(cors());
+
+// Specific origins
+// let allowedOrigins = ['http://localhost:8080',];
+// app.use(cors({
+// origin: (origin, callback) => {
+//   if(!origin) return callback(null, true);
+//   if(allowedOrigins.indexOf(origin) === -1){
+//     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//     return callback(new Error(message ), false);
+//   }
+//   return callback(null, true);
+// }
+// }));
 
 let auth = require('./auth')(app);
 
