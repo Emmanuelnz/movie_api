69958678ba3d21f095f1f3f5c35adf3313e509d6
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-26 01:54:16 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,30 +5,31 @@ const jwt = require('jsonwebtoken'),
 
   require('./passport');
 
-let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
-    subject: user.Username, //Username being encoded in the JWT
-    expiresIn: '7d', //Time token will take to expire
-    algorithm: 'HS256', //algorithm used to “sign” or encode the values of the JWT
-  });
-};
+  let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+      subject: user.Username, // Username being enoded in JWT
+      expiresIn: '7d', // Time token will take to expire
+      algorithm: 'HS256' // Algorithm used to “sign” or encode the values of the JWT
+    });
+  }
 
+// POST login
 module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
           message: 'Something is not right',
-          user: user,
+          user: user
         });
       }
-       req.login(user, { session: false }, (error) => {
-         if (error) {
-           res.send(error);
-         }
-         let token = generateJWTToken(user.toJSON());
-         return res.json({ user, token });
-       });
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
     })(req, res);
   });
-};
+}
