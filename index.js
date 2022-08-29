c8165c70dde1187c4fc783935c117c3600476014
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-29 00:15:11 -0400

    auth

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
 
 // temp rem - passport.authenticate('jwt', {session: false}),
 // GET - List of all movies
-app.get('/movies',
+app.get('/movies', passport.authenticate('jwt', {session: false}),
 (req, res) => {
   Movies.find()
     .then((movies) => {
