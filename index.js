499e953c8c048cafb986a171777e028bbbac2a4f
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-31 02:07:43 -0400

    added single user endpoint

--- index.js
+++ index.js
@@ -200,7 +200,18 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
     });
 });
 
-// temp rem - passport.authenticate('jwt', {session: false}),
+// GET - Single user
+app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) =>{
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // GET - List of all movies
 app.get('/movies', passport.authenticate('jwt', {session: false}),
 (req, res) => {
